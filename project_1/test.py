c5b5566d0301f2bb0f52ff0c4ca7229882bfbe44
Lora <Larius77@mail.ru>
2022-09-12 09:27:02 +0300

    Создание структуры папок

---
+++ project_1/test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#arr, step = np.linspace(-6, 21, 60, endpoint=False, retstep=True)
+#print(step)
+#print(round(step,2))
+
+
+
+
+simplelist = [19, 242, 14, 152, 142, 1000]
+print(np.mean(simplelist))
